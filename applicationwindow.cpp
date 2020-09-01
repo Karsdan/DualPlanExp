0220d66b225b8bc4af45c341fa31645ac3710313
Karsdan <bukovskiy.pavel@gmail.com>
2020-09-01 13:10:29 +0300

    Add MDI

--- applicationwindow.cpp
+++ applicationwindow.cpp
@@ -3,9 +3,50 @@
 ApplicationWindow::ApplicationWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    mdiArea = new QMdiArea();
+    mdiArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    mdiArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+    connect(mdiArea, &QMdiArea::subWindowActivated, this, &ApplicationWindow::updateMenu);
+
+    setWindowTitle("Двухфакторный планированный эксперимент (DualPlanExp)");
 }
 
 ApplicationWindow::~ApplicationWindow()
 {
 }
+
+void ApplicationWindow::updateMenu()
+{
+//    bool hasMdiChild = (activeMdiChild != 0);
+
+//    actionSave->setEnabled(hasMdiChild);
+//    actionSaveAs->setEnabled(hasMdiChild);
+
+//#ifndef QT_NO_CLIPBOERD
+//    actionPaste->setEnabled(hasMdiChild);
+//#endif
+//    actionClose->setEnabled(hasMdiChild);
+//    actionCloseAll->setEnabled(hasMdiChild);
+//    actionNext->setEnabled(hasMdiChild);
+//    actionPrevious->setEnabled(hasMdiChild);
+//    actionWindowMenuSeparator->setEnabled(hasMdiChild);
+
+//#ifndef QT_NO_CLIPBOARD
+//    bool hasSelection = (activeMdiChild() &&
+//                         activeMdiChild()->textCursor().hasSelection());
+//    actionCut->setEnabled(hasSelection);
+//    actionCopy->setEnabled(hasSelection);
+//#endif
+}
+
+class ActiveMdiSubWindowFunctor {
+public:
+    explicit ActiveMdiSubWindowFunctor(QMdiArea *mdiArea, QMdiSubWindow *activeWindow) : m_mdiArea(mdiArea), m_activeWindow(activeWindow) {}
+    void operator()() const { m_mdiArea->setActiveSubWindow(m_activeWindow); }
+
+private:
+    QMdiArea *m_mdiArea;
+    QMdiSubWindow *m_activeWindow;
+};
 
--- applicationwindow.h
+++ applicationwindow.h
@@ -4,6 +4,13 @@
 #include <QMainWindow>
 #include <QtWidgets>
 
+QT_BEGIN_NAMESPACE
+class QAction;
+class QMenu;
+class QMdiArea;
+class QMdiSubWindow;
+QT_END_NAMESPACE
+
 class ApplicationWindow : public QMainWindow
 {
     Q_OBJECT
@@ -11,5 +18,11 @@ class ApplicationWindow : public QMainWindow
 public:
     ApplicationWindow(QWidget *parent = nullptr);
     ~ApplicationWindow();
+
+private:
+    QMdiArea *mdiArea;
+
+private slots:
+    void updateMenu();
 };
 #endif // APPLICATIONWINDOW_H
