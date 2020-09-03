d56359b67ae03cae4a0f875784eb272612aa702d
Karsdan <bukovskiy.pavel@gmail.com>
2020-09-03 12:20:11 +0300

    Добавление интерфейса в PlanExpDialog

--- planexpdialog.cpp
+++ planexpdialog.cpp
@@ -12,4 +12,70 @@ PlanExpDialog::PlanExpDialog(QWidget *parent)
     isUntitled = true;
     curFile = tr("PlanExp %1").arg(sequenceNumber++);
     setWindowTitle(curFile + "[*]");
+
+    initPlanDlg();
+}
+
+void PlanExpDialog::initPlanDlg()
+{
+    levelBox = new QGroupBox();
+    levelBox->setTitle(tr("Уровни варьируемых факторов"));
+
+    lblX1 = new QLabel();
+    lblX1->setText(tr("X1"));
+
+    lblX2 = new QLabel();
+    lblX2->setText(tr("X2"));
+
+    lblLowLevel = new QLabel();
+    lblLowLevel->setText(tr("Нижний"));
+
+    lblTopLevel = new QLabel();
+    lblTopLevel->setText(tr("Верхний"));
+
+    lblMidLevel = new QLabel();
+    lblMidLevel->setText(tr("Основной"));
+
+    lblRangeVar = new QLabel();
+    lblRangeVar->setText(tr("Интервал\nварьирования"));
+
+    boxLowLevelX1 = new QLineEdit();
+    boxLowLevelX2 = new QLineEdit();
+
+    boxTopLevelX1 = new QLineEdit();
+    boxTopLevelX2 = new QLineEdit();
+
+    boxMidLevelX1 = new QLineEdit();
+    boxMidLevelX2 = new QLineEdit();
+    boxMidLevelX1->setEnabled(false);
+    boxMidLevelX2->setEnabled(false);
+
+    boxRangeX1 = new QLineEdit();
+    boxRangeX2 = new QLineEdit();
+    boxRangeX1->setEnabled(false);
+    boxRangeX2->setEnabled(false);
+
+
+    //LAYOUTS
+
+    QVBoxLayout *windowBoxLayout = new QVBoxLayout(this);
+    windowBoxLayout->addWidget(levelBox);
+    windowBoxLayout->addStretch(1);
+
+    QGridLayout *levelBoxLayout = new QGridLayout(levelBox);
+    levelBoxLayout->addWidget(lblX1, 0, 1);
+    levelBoxLayout->addWidget(lblX2, 0, 2);
+    levelBoxLayout->addWidget(lblLowLevel, 1, 0);
+    levelBoxLayout->addWidget(boxLowLevelX1, 1, 1);
+    levelBoxLayout->addWidget(boxLowLevelX2, 1, 2);
+    levelBoxLayout->addWidget(lblTopLevel, 2, 0);
+    levelBoxLayout->addWidget(boxTopLevelX1, 2, 1);
+    levelBoxLayout->addWidget(boxTopLevelX2, 2, 2);
+    levelBoxLayout->addWidget(lblMidLevel, 3, 0);
+    levelBoxLayout->addWidget(boxMidLevelX1, 3, 1);
+    levelBoxLayout->addWidget(boxMidLevelX2, 3, 2);
+    levelBoxLayout->addWidget(lblRangeVar, 4, 0);
+    levelBoxLayout->addWidget(boxRangeX1, 4, 1);
+    levelBoxLayout->addWidget(boxRangeX2, 4, 2);
+
 }
--- planexpdialog.h
+++ planexpdialog.h
@@ -16,6 +16,32 @@ private:
     QString curFile;
     bool isUntitled;
 
+private:
+
+    void initPlanDlg();
+
+    QGroupBox *levelBox;
+//    QTableWidget *tableLevel;
+    QLabel *lblX1, *lblX2, *lblLowLevel, *lblTopLevel, *lblMidLevel, *lblRangeVar;
+    QLineEdit *boxLowLevelX1, *boxTopLevelX1, *boxMidLevelX1, *boxRangeX1;
+    QLineEdit *boxLowLevelX2, *boxTopLevelX2, *boxMidLevelX2, *boxRangeX2;
+
+    QGroupBox *planeBox;
+
+    QGroupBox *modelBox;
+    QLabel *lblModel;
+    QLineEdit *boxModel;
+
+    QGroupBox *importanceBox;
+    QLabel *lblReprDispertion, *lblReprStep, *lblTableStudent;
+    QLineEdit *boxReprDispertion, *boxReprStep, *boxTableStudent;
+
+
+    QGroupBox *adequacyBox;
+    QLabel *lblAdeqDispertion, *lblAdeqStep, *lblTableFisher, *lblCalcFisher;
+    QLineEdit *boxAdeqDispertion, *boxAdeqStep, *boxTableFisher, *boxCalcFisher;
+
+
 };
 
 #endif // PLANEXPDIALOG_H
